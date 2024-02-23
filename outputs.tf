b4c7d22ed790623a0428c39fc996ac4be8285f42
keyusuf <mrkenanyusuf@gmail.com>
2024-02-22 22:18:01 -0500

    final commit

--- outputs.tf
+++ outputs.tf
@@ -4,21 +4,17 @@ output "resource_group_name" {
   description = "The name of the Azure resource group provisioned."
 }
 
-
 output "vm_name" {
   value       = azurerm_linux_virtual_machine.webserver.name
   description = "The name of the virtual machine deployed as a web server in Azure."
 }
-
 
 output "nic_name" {
   value       = azurerm_network_interface.webserver.name
   description = "The name of the network interface attached to the web server in Azure."
 }
 
-
 output "public_ip" {
   value       = azurerm_linux_virtual_machine.webserver.public_ip_address
   description = "The public IP address of the web server deployed in Azure."
 }
-
